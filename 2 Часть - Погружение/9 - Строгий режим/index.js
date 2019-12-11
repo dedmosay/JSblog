fa0192fb639e81b9b945ee56c838674c867fc51a
ilinoa <ilinoa90@gmail.com>
2019-12-11 16:55:33 +0300

    Вызов функции, метода

--- 2 Часть - Погружение/9 - Строгий режим/index.js
+++ 2 Часть - Погружение/9 - Строгий режим/index.js
@@ -1,4 +1,11 @@
-"use struct";                                           // Активизировать строгий режим
+// Предисловие
+/**
+ * 1 Этот режим внедрен в ES5 для генерации ошибок, в то время как они могут обрабатываться не гласно
+ * 2 В не строгом режиме контекст может быть глобальным (window), а в строгом режиме  -  неопределенным (undefined)
+ */
+
+
+"use strict";                                           // Активизировать строгий режим
 
 function assert(value, desc) {
     var li = document.createElement("li");
@@ -7,3 +14,13 @@ function assert(value, desc) {
     document.getElementById("results").appendChild(li);
 }
 
+function infilrate (person) {
+
+    assert (person === 'user', "The person is a user");         // person и arguments[0] имеют одинаковое значение
+    assert (arguments[0] === 'user', "The person is a user");   
+
+    arguments[0] = 'agent';                                         // Первый аргумент изменен
+
+    assert(arguments[0] === 'agent', "The person is a agent");      // Значение person не изменилось
+}
+infilrate('user');
